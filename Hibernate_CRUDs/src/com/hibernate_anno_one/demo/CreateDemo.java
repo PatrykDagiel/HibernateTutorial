6baf83909a2059ff719f6d245110b591748be4e6
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-07-26 15:54:52 +0200

    213 finished.

--- Hibernate_CRUDs/src/com/hibernate_anno_one/demo/CreateDemo.java
+++ Hibernate_CRUDs/src/com/hibernate_anno_one/demo/CreateDemo.java
@@ -24,18 +24,33 @@ public class CreateDemo {
             System.out.println("Creating new instructor and inst_details object");
             // create and associate objects
             Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@lub2code.com");
+            Instructor tempInstructor_2 = new Instructor("Jan", "Kowalski", "XYZ");
             InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "luv 2 code");
+            InstructorDetail tempInstructorDetail_2 = new InstructorDetail("http://www.dummy.com", "Test");
 
             //associate objects
             tempInstructor.setInstructorDetail(tempInstructorDetail);
+            tempInstructor_2.setInstructorDetail(tempInstructorDetail_2);
 
             // start a transaction
             session.beginTransaction();
 
+            // Get instructor by primary key / id
+            int theId = 4;
+            Instructor delInstructor = session.get(Instructor.class, theId);
+            System.out.println("Found instructor: " + delInstructor);
+
+            // delete the instructor
+            if(delInstructor != null) {
+                session.delete(delInstructor);
+                System.out.printf("Removal done");
+            }
+
             // save instructor
             // This will also save details object because of CascadeType.ALL
-            System.out.println("Saving instructor: " + tempInstructor);
-            session.save(tempInstructor);
+//            System.out.println("Saving instructor: " + tempInstructor);
+//            session.save(tempInstructor);
+//            session.save(tempInstructor_2);
 
             // commit transaction
             session.getTransaction().commit();
