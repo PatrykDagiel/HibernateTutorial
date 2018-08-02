fb4905f0fd7413c77dfa0d75c2263f7a921de145
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-08-02 16:39:06 +0200

    Past 227. v2

---
+++ Hibernate_CRUDs/src/com/hibernate_one_to_many/demo/OneToManyDemo.java
@@ -0,0 +1,44 @@
+package com.hibernate_one_to_many.demo;
+
+import com.hibernate_one_to_many.entity.Course;
+import com.hibernate_one_to_many.entity.Instructor;
+import com.hibernate_one_to_many.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class OneToManyDemo {
+    public static void main(String[] args) {
+
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+        //create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // use the session object to save Java object
+            System.out.println("Creating new instructor and inst_details object");
+            // create and associate objects
+            Instructor tempInstructor_2 = new Instructor("Jan", "Kowalski", "XYZ");
+            InstructorDetail tempInstructorDetail_2 = new InstructorDetail("http://www.dummy.com", "Test");
+
+            //associate objects
+            tempInstructor_2.setInstructorDetail(tempInstructorDetail_2);
+
+            // start a transaction
+            session.beginTransaction();
+
+            session.save(tempInstructor_2);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done correctly");
+        } finally {
+            factory.close();
+        }
+}}
