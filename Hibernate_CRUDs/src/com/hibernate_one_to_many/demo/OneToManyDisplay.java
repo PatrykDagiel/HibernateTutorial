8ed5a7ab2c199f94deae23226612a9945ab744b8
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-08-03 10:14:41 +0200

    Past 230 / OtM.

--- Hibernate_CRUDs/src/com/hibernate_one_to_many/demo/OneToManyDisplay.java
+++ Hibernate_CRUDs/src/com/hibernate_one_to_many/demo/OneToManyDisplay.java
@@ -7,7 +7,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class OneToManyDemo {
+public class OneToManyDisplay {
     public static void main(String[] args) {
 
         //create session factory
@@ -21,33 +21,23 @@ public class OneToManyDemo {
         Session session = factory.getCurrentSession();
 
         try {
-//            // use the session object to save Java object
-//            System.out.println("Creating new instructor and inst_details object");
-//            // create and associate objects
-//            Instructor tempInstructor_2 = new Instructor("Jan", "Kowalski", "XYZ");
-//            InstructorDetail tempInstructorDetail_2 = new InstructorDetail("http://www.dummy.com", "Test");
-//            //associate objects
-//            tempInstructor_2.setInstructorDetail(tempInstructorDetail_2);
 
             // start a transaction
             session.beginTransaction();
 
-            // get Instructor from DB
             int theId = 1;
-            Instructor tempInstructor = session.get(Instructor.class, theId);
+            Instructor theInstructor = session.get(Instructor.class, theId);
 
-            // create some courses
-            Course tempCourse1 = new Course("Air guitar");
-            Course tempCourse2 = new Course("The pinball Masterclass");
+            System.out.println("Instructor:  " + theInstructor);
 
-            // add courses to Instructor
-            tempInstructor.add(tempCourse1);
-            tempInstructor.add(tempCourse2);
+            // get courses
+            System.out.println("Courses: " + theInstructor.getCourses());
 
+            // course Removal
+            int theId2 = 2;
+            Course course = session.get(Course.class, theId2);
 
-            // save the couses
-            session.save(tempCourse1);
-            session.save(tempCourse2);
+            session.delete(course);
 
             // commit transaction
             session.getTransaction().commit();
