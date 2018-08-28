a039eba989d4ee7712a064c9b00d08c6b929a035
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-08-28 15:57:23 +0200

    254 done.

--- Hibernate_CRUDs/src/com/many_to_many/demo/OneToManyDemo.java
+++ Hibernate_CRUDs/src/com/many_to_many/demo/OneToManyDemo.java
@@ -1,9 +1,6 @@
 package com.many_to_many.demo;
 
-import com.many_to_many.entity.Course;
-import com.many_to_many.entity.Instructor;
-import com.many_to_many.entity.InstructorDetail;
-import com.many_to_many.entity.Review;
+import com.many_to_many.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -18,6 +15,7 @@ public class OneToManyDemo {
                 .addAnnotatedClass(InstructorDetail.class)
                 .addAnnotatedClass(Course.class)
                 .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
         //create session
         Session session = factory.getCurrentSession();
@@ -28,17 +26,25 @@ public class OneToManyDemo {
 
             // create a course
             Course tempCourse = new Course("Pacman - How to Score One Million Points");
-            //add some reviews
-            tempCourse.addReview(new Review("Great course"));
-            tempCourse.addReview(new Review("Great course ... really nice"));
-            tempCourse.addReview(new Review("Great course .. 4/5"));
-
-            System.out.println("Saving the course: " + tempCourse);
-            // save and commit
+            System.out.printf("Saving course completed:: " + tempCourse);
             session.save(tempCourse);
-            session.getTransaction().commit();
 
+            // create students
+            Student tempStudent1 = new Student("John", "Kow", "mail1@mail.com");
+            Student tempStudent2 = new Student("Tomek","Now", "tomek@mail.com");
+
+            // add students to the course
+            tempCourse.addStudent(tempStudent1);
+            tempCourse.addStudent(tempStudent2);
+
+            // save students
+            session.save(tempStudent1);
+            session.save(tempStudent2);
+            System.out.println("Students added: " + tempStudent1 + "    " + tempStudent2);
+
+            session.getTransaction().commit();
             System.out.println("Done correctly");
+
         } finally {
             factory.close();
         }
